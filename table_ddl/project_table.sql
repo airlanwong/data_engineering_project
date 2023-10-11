0794cd7d9a7c2e89c6de93da63681879c39cfa81
alan <alan.wonghaolin@gmail.com>
2023-10-11 11:57:57 +0800

    buildspec

---
+++ table_ddl/project_table.sql
@@ -0,0 +1,20 @@
+create table if not exists public.project_table
+(
+gender VARCHAR(5),
+NationalITy VARCHAR(5),
+PlaceofBirth VARCHAR(10),
+StageID VARCHAR(10),
+GradeID VARCHAR(10),
+SectionID VARCHAR(5),
+Topic VARCHAR(10),
+Semester VARCHAR(10),
+Relation VARCHAR(10),
+raisedhands Integer,
+VisITedResources Integer,
+AnnouncementsView Integer,
+Discussion Integer,
+ParentAnsweringSurvey VARCHAR(10),
+ParentschoolSatisfaction VARCHAR(10),
+StudentAbsenceDays VARCHAR(10),
+Class VARCHAR(10)
+)
